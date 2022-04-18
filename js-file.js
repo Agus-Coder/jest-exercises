8045e68b582c833acf51e151e7f1141fd98d1d30
Agus-Coder <93748858+Agus-Coder@users.noreply.github.com>
2022-04-18 20:46:16 -0300

    Fin primer ejercicio

--- js-file.js
+++ js-file.js
@@ -1,17 +1,10 @@
-function capitalize(string){
- let firstLetter = string.charAt(0)
- const firstLetterCap = firstLetter.toUpperCase()   
- let restOfString = string.slice(1) //con un solo numero devuelve desde ese numero en adelante
+function capitalize(string) {
+    let firstLetter = string.charAt(0)
+    const firstLetterCap = firstLetter.toUpperCase()
+    let restOfString = string.slice(1) //con un solo numero devuelve desde ese numero en adelante
 
- let result = ''
-
- if(string.charAt(1) === " "){
-    result = firstLetterCap.concat(' ',restOfString)
+    let result = firstLetterCap.concat(restOfString)
     return result
- }else{
-     result = result = firstLetterCap.concat(restOfString)
-     return result
- }
 
 }
 
--- js-file.test.js
+++ js-file.test.js
@@ -1,5 +1,17 @@
 const capitalize = require('./js-file')
 
-test('first', () => {
+test('first test', () => {
     expect(capitalize('first')).toBe('First')
+})
+
+test('second test', () => {
+    expect(capitalize('this is the second test')).toBe('This is the second test')
+})
+
+test('Third test', () => {
+    expect(capitalize('a third test is always a good idea')).toBe('A third test is always a good idea')
+})
+
+test('Fourth test', () => {
+    expect(capitalize('First')).toBe('First')
 })

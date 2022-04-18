cae634f9c9812c4ff34f984c80ecdc72ebf6f909
Agus-Coder <93748858+Agus-Coder@users.noreply.github.com>
2022-04-18 19:58:29 -0300

    Primer version ejercicio Capitalize

--- js-file.js
+++ js-file.js
@@ -1,3 +1,14 @@
 function capitalize(string){
-    
+ let firstLetter = string.chartAt(0)
+ const firstLetterCap = firstLetter.capitalize()   
+ let restOfString = string.slice(0,1)
+
+ let result = ''
+
+ if(string.chartAt(0) === " "){
+    result = firstLetterCap.concat(' ',restOfString)
+ }else{
+     result = result = firstLetterCap.concat(restOfString)
+ }
+
 }
